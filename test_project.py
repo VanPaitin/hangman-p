35da816f3695da915dce15f8cb7a2cc9b0787bff
Mayowa Pitan <mayowasolomon@gmail.com>
2026-07-13 06:20:47 +0100

    Remove hangman alias and bump version

--- test_project.py
+++ test_project.py
@@ -8,19 +8,28 @@ from hangman_p import level
 from hangman_p.utilities import get_valid_choice, name_validator, VALID
 
 
-
 def test_game_actions():
-    assert game_actions() == ['p', 'P', 'play', 'i', 'I', 'instruction', 'q', 'Q', 'quit', 'l', 'L', 'load']
+    assert game_actions() == [
+        "p",
+        "P",
+        "play",
+        "i",
+        "I",
+        "instruction",
+        "q",
+        "Q",
+        "quit",
+        "l",
+        "L",
+        "load",
+    ]
 
 
 def test_get_level(monkeypatch, capsys):
     config = level.Level.config
     # test single player
-    difficulty = choice(['1', '2', '3'])
-    answers = iter([
-        '2', # option= '2' => single player
-        difficulty
-    ])
+    difficulty = choice(["1", "2", "3"])
+    answers = iter(["2", difficulty])  # option= '2' => single player
 
     monkeypatch.setattr("builtins.input", lambda _: next(answers))
 
@@ -28,45 +37,48 @@ def test_get_level(monkeypatch, capsys):
 
     out = capsys.readouterr().out
 
-    assert f'Welcome Mayowa, In this level, you will have {lvl.chances} chances to guess' in out
+    assert (
+        f"Welcome Mayowa, In this level, you will have {lvl.chances} chances to guess"
+        in out
+    )
 
     assert lvl.difficulty == difficulty
-    assert lvl.player == 'Mayowa'
+    assert lvl.player == "Mayowa"
     assert lvl.challenger == None
-    assert lvl.min == config[difficulty]['min']
-    assert lvl.max == config[difficulty]['max']
-    assert lvl.chances == config[difficulty]['chances']
+    assert lvl.min == config[difficulty]["min"]
+    assert lvl.max == config[difficulty]["max"]
+    assert lvl.chances == config[difficulty]["chances"]
 
     # test multiplayer
-    answers = iter([
-        '1', # option= '1' => multi player
-        '1', # difficulty of 1
-        'Femi',
-        'Mayowa'
-    ])
+    answers = iter(
+        ["1", "1", "Femi", "Mayowa"]  # option= '1' => multi player  # difficulty of 1
+    )
     monkeypatch.setattr("builtins.input", lambda _: next(answers))
-    monkeypatch.setattr(level, 'askpass', lambda _: 'secret')
+    monkeypatch.setattr(level, "askpass", lambda _: "secret")
 
-    lvl = get_level('Mayowa')
+    lvl = get_level("Mayowa")
     out = capsys.readouterr().out
 
-    assert f'Welcome Femi, In this level, you will have {lvl.chances} chances to guess' in out
-    assert lvl.difficulty == '1'
-    assert lvl.player == 'Femi'
-    assert lvl.challenger == 'Mayowa'
-    assert lvl.min == config['1']['min']
-    assert lvl.max == config['1']['max']
-    assert lvl.chances == config['1']['chances']
+    assert (
+        f"Welcome Femi, In this level, you will have {lvl.chances} chances to guess"
+        in out
+    )
+    assert lvl.difficulty == "1"
+    assert lvl.player == "Femi"
+    assert lvl.challenger == "Mayowa"
+    assert lvl.min == config["1"]["min"]
+    assert lvl.max == config["1"]["max"]
+    assert lvl.chances == config["1"]["chances"]
 
 
 def test_get_challenger(monkeypatch):
-    answers = iter(['Wrongname', 'incorrect', 'invalid', 'Mayowa'])
+    answers = iter(["Wrongname", "incorrect", "invalid", "Mayowa"])
 
     monkeypatch.setattr("builtins.input", lambda _: next(answers))
     # It should only return any of the passed in arguments
-    challenger = get_challenger('Mayowa', 'Tunde')
+    challenger = get_challenger("Mayowa", "Tunde")
 
-    assert challenger == 'Mayowa'
+    assert challenger == "Mayowa"
 
 
 def test_name_validator():
